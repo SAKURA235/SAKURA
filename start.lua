4bcc351400c8c03e4bc174d0e4a04b581ff23714
SAKURA235 <61616605+SAKURA235@users.noreply.github.com>
2020-02-29 11:58:08 +0300

    Add files via upload

--- start.lua
+++ start.lua
@@ -70,8 +70,6 @@ RunTshake:write([[
 #!/usr/bin/env bash
 cd $HOME/Tshake
 token="]]..database:get(Server_Tshake.."Token_Tshake")..[["
-rm -fr Tshake.lua
-wget "https://raw.githubusercontent.com/tshakeabas/Tshake/master/Tshake.lua"
 while(true) do
 rm -fr ../.telegram-cli
 ./tg -s ./Tshake.lua -p PROFILE --bot=$token
